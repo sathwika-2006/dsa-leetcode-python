2cacbae40f7f18d56867fcefa12471c45323d897
Sathwika chinnamula <chinnamulasathwika@gmail.com>
2026-07-30 23:57:41 +0530

    Add files via upload

---
+++ print_factors.py
@@ -0,0 +1,13 @@
+from math import sqrt
+def print_factors(n):
+    result=[]
+    num=n
+    for i in range(1,int(sqrt(num))+1):
+        if num%i==0:
+            result.append(i)
+            if num//i!=i:
+                result.append(num//i)
+    result.sort()
+    return result
+
+print(print_factors(36))
