de5597bc01ecf89f378ef62ff4627eb0c5000188
Sathwika chinnamula <chinnamulasathwika@gmail.com>
2026-09-07 17:14:11 +0530

    Add files via upload

---
+++ 1.two_sum.py
@@ -0,0 +1,12 @@
+# Leetcode-1 Two sum 
+class Solution(object):
+    def twoSum(self, nums, target):
+        seen={}
+        for i in range(len(nums)):
+            num=nums[i]
+            needed=target-num
+            if needed in seen:
+                return [seen[needed],i]
+            else:
+                seen[num]=i
+        
