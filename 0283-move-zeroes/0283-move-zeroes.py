cb136e22f1e0ab02df9d6a6f8bb32d07a2e4f56e
Sathwika chinnamula <chinnamulasathwika@gmail.com>
2026-09-11 14:43:07 +0530

    Time: 2 ms (92.77%), Space: 13.7 MB (20.11%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        i=0
+        for j in range(0,len(nums)):
+            if nums[j]!=0:
+                nums[i]=nums[j]
+                i+=1
+        while i<len(nums):
+            nums[i]=0
+            i+=1
+        return nums
