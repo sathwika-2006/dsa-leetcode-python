67fb240d16239706bedc2fd022ac3e477a96ccf5
Sathwika chinnamula <chinnamulasathwika@gmail.com>
2026-09-17 23:03:32 +0530

    Time: 22 ms (9.22%), Space: 14.5 MB (6.93%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        candidate=None
+        count=0
+        for i in range(len(nums)):
+            if count==0:
+                candidate=nums[i]
+            if nums[i]==candidate:
+                count+=1
+            else:
+                count-=1
+        return candidate
+        
